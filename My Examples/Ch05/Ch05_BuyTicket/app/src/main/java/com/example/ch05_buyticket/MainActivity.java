7d105e75971c9e8ec2987c63aad225ee0bb49bed
TK-yunyou <2578963817@qq.com>
2021-08-10 16:58:13 +0800

    update func show

--- My Examples/Ch05/Ch05_BuyTicket/app/src/main/java/com/example/ch05_buyticket/MainActivity.java
+++ My Examples/Ch05/Ch05_BuyTicket/app/src/main/java/com/example/ch05_buyticket/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ch05_buyticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    原始版本
+    public void show(View v){
+        TextView txv = findViewById(R.id.txv);
+        RadioGroup ticketType = findViewById(R.id.ticketType);
+
+        switch(ticketType.getCheckedRadioButtonId()){
+            case R.id.adult:
+                txv.setText("买全票");
+                break;
+            case R.id.child:
+                txv.setText("买半票");
+                break;
+            case R.id.senior:
+                txv.setText("买敬老票");
+                break;
+        }
+    }
+     */
+
+    //简化版本
+    public void show(View v){
+        TextView txv = findViewById(R.id.txv);
+        RadioGroup ticketType = findViewById(R.id.ticketType);
+
+        int id = ticketType.getCheckedRadioButtonId();
+        RadioButton select = findViewById(id);
+        txv.setText("买" + select.getText());
     }
 }
